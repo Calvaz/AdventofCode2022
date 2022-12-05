94cd3a01a8dcabfa3e3b3b3397f85fc1936d9fd0
calvaz <andreacavo92@gmail.com>
2022-12-05 22:22:30 +0100

    solved second part day4

--- day4/src/puzzle_solver.rs
+++ day4/src/puzzle_solver.rs
@@ -17,7 +17,7 @@ impl PuzzleSolver {
             let elf_assignments: Vec<&str> = l.split(',').collect();
             let first_elf_task: (u16, u16) = Self::split_on_bar(elf_assignments[0]);
             let second_elf_task: (u16, u16) = Self::split_on_bar(elf_assignments[1]);
-            Self::assignments_condition_first_part(&mut assignments_contains, first_elf_task, second_elf_task);
+            Self::assignments_condition_second_part(&mut assignments_contains, first_elf_task, second_elf_task);
         }
 
         assignments_contains
@@ -26,6 +26,19 @@ impl PuzzleSolver {
     fn assignments_condition_first_part(result: &mut i32, first_elf_task: (u16, u16), second_elf_task: (u16, u16)) {
         if (first_elf_task.0 >= second_elf_task.0) && (first_elf_task.1 <= second_elf_task.1) ||
             (second_elf_task.0 >= first_elf_task.0) && (second_elf_task.1 <= first_elf_task.1) {
+                *result += 1;
+            }
+    }
+
+    fn assignments_condition_second_part(
+        result: &mut i32, 
+        (first_elf_start, first_elf_end): (u16, u16), 
+        (second_elf_start, second_elf_end): (u16, u16)
+    ) {
+        if (first_elf_start <= second_elf_start && first_elf_end >= second_elf_start ||
+            first_elf_end >= second_elf_end && first_elf_start <= second_elf_end) ||
+            second_elf_start <= first_elf_start && second_elf_end >= first_elf_start ||
+            second_elf_end >= first_elf_end && second_elf_start <= first_elf_end { 
                 *result += 1;
             }
     }
@@ -39,5 +52,23 @@ impl PuzzleSolver {
 #[cfg(test)]
 mod tests {
     
+    use crate::PuzzleSolver;
+
+    #[test]
+    fn condition_second_part() {
+        let mut first_elf = (2, 4);
+        let mut second_elf = (6, 8);
+        let mut result: i32 = 0;
+
+        PuzzleSolver::assignments_condition_second_part(&mut result, first_elf, second_elf);
+        assert_eq!(0, result);
+
+        first_elf = (2, 3);
+        second_elf = (4, 5);
+        result = 0;
+
+        PuzzleSolver::assignments_condition_second_part(&mut result, first_elf, second_elf);
+        assert_eq!(0, result);
+    }
 }
 
